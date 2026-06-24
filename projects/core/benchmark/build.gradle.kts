1581e3e9949433bac6a89393844bc37abab19798
Dayan Ruben <mail@dayanruben.com>
2026-06-24 15:13:22 +0000

    chore: bump Kotlin EAP to 2.4.20-Beta1

--- projects/core/benchmark/build.gradle.kts
+++ projects/core/benchmark/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    kotlin("plugin.allopen") version "2.4.0-RC2"
+    kotlin("plugin.allopen") version "2.4.20-Beta1"
     alias(libs.plugins.benchmark)
 }
 
