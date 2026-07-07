41f4393276f38c0941c6011c21ff0d7d507c3d4a
Dayan Ruben <mail@dayanruben.com>
2026-07-07 14:11:51 +0000

    chore: bump Kotlin EAP to 2.4.10-RC2

--- projects/core/benchmark/build.gradle.kts
+++ projects/core/benchmark/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    kotlin("plugin.allopen") version "2.4.10-RC"
+    kotlin("plugin.allopen") version "2.4.10-RC2"
     alias(libs.plugins.benchmark)
 }
 
